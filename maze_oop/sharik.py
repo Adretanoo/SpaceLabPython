5c8cc6b26007213f29ff37f54878ee40f7b699ba
Adretanoo <adriantegzza@gmail.com>
2025-03-27 17:27:07 +0200

    add wall crash treatment

--- maze_oop/sharik.py
+++ maze_oop/sharik.py
@@ -6,21 +6,25 @@ class Sharik:
         self.arr[sharik[1]] = ' '
         self.arr[sharik[1] + 1] = sharik[0]
         sharik[1] = sharik[1] + 1
+        print("\nШарік на правельному шляху!")
 
     def move_left(self, sharik):
         self.arr[sharik[1]] = ' '
         self.arr[sharik[1] - 1] = sharik[0]
         sharik[1] = sharik[1] - 1
+        print("\nШарік на правельному шляху!")
 
     def move_down(self, sharik):
         self.arr[sharik[1]] = ' '
         self.arr[sharik[1] + 20] = sharik[0]
         sharik[1] = sharik[1] + 20
+        print("\nШарік на правельному шляху!")
 
     def move_up(self, sharik):
         self.arr[sharik[1]] = ' '
         self.arr[sharik[1] - 20] = sharik[0]
         sharik[1] = sharik[1] - 20
+        print("\nШарік на правельному шляху!")
 
     def display(self):
-        return ' '.join(self.arr)
+        print(' '.join(self.arr))
